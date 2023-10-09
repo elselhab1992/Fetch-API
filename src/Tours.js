e20dde2e0f11806c2923f14d26e5bf6e7dfaa9dd
Mohamed Elselhab <momanchester1992@gmail.com>
2023-10-09 14:53:32 +0100

    fetch data from api

---
+++ src/Tours.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, handleDelete }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+        <div>
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour} handleDelete={handleDelete} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
